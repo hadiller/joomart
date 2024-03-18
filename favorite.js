8b12932a26d39885ed5599e2d20a1f733d7cce64
Hadiller <almazbekov.hadi@gmail.com>
2024-03-18 16:08:49 +0600

    feat: filtered sneakers on favorite page

--- favorite.js
+++ favorite.js
@@ -1,6 +1,7 @@
-function fetchData(api) {
-  return fetch(api)
+function fetchData(url) {
+  return fetch(url)
     .then((response) => response.json())
+    .then((data) => data.filter((sneaker) => sneaker.isLike === true))
     .catch((error) => console.error("У тебя ошибка, исправляй", error));
 }
 
@@ -9,78 +10,34 @@ function renderSneakers(data) {
   wrapper.innerHTML = data
     .map((item) => {
       return `          
-            <div class="sneakers-card">
-            <button class="sneakers__card-like">
-              <img src="./images/like-1.svg" alt="" class="card-like" data-sneaker-id="${item.id}" data-fav-id="-1"/>
-            </button>
-            <img
-              src="../images/${item.imageUrl}"
-              alt=""
-              class="sneakers__card-img"
-            />
-            <h4 class="sneakers__card-title">
-              ${item.title}
-            </h4>
-            <div class="sneakers__card-actions">
-              <div class="sneakers__action-price">
-                <p>Цена</p>
-                <b>${item.price}$</b>
-              </div>
-              <button class="sneakers__actions-cart">
-                <img src="./images/plus.svg" alt="" />
-              </button>
+          <div class="sneakers-card">
+          <button class="sneakers__card-like">
+            <img src="../images/like-2.svg" alt="" class="card-like" data-sneaker-id="${item.id}"/>
+          </button>
+          <img
+            src="../images/${item.imageUrl}"
+            alt=""
+            class="sneakers__card-img"
+          />
+          <h4 class="sneakers__card-title">
+            ${item.title}
+          </h4>
+          <div class="sneakers__card-actions">
+            <div class="sneakers__action-price">
+              <p>Цена</p>
+              <b>${item.price}$</b>
             </div>
-          </div>`;
+            <button class="sneakers__actions-cart">
+              <img src="./images/plus.svg" alt="" />
+            </button>
+          </div>
+        </div>`;
     })
     .join("");
 }
 
-let cardLike = document.querySelector(".card-like");
-
-function addLike(sneakersID) {
-  return fetch("https://0348dd6250bed296.mokky.dev/ttttt", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ like: sneakersID }),
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log("error: ", error));
-}
-
-document.addEventListener("click", (event) => {
-  if (event.target.classList.contains("card-like")) {
-    if (event.target.src === "http://127.0.0.1:5500/images/like-1.svg") {
-      event.target.src = "http://127.0.0.1:5500/images/like-2.svg";
-      const sneakerId = event.target.dataset.sneakerId;
-      console.log(sneakerId);
-      addLike(sneakerId).then((json) => (event.target.dataset.favId = json.id));
-    } else {
-      event.target.src = "http://127.0.0.1:5500/images/like-1.svg";
-      const likeId = event.target.dataset.favId;
-      removeLike(likeId);
-    }
-  }
-});
-
-function renderSneakers(likedSneakers) {
-  let favImgs = document.querySelectorAll(".card-like");
-
-  favImgs.forEach((favImg) => {
-    let sneakerId = favImg.dataset.sneakerId;
-    if (likedSneakers.some((sneaker) => sneaker.sneakerId === sneakerId)) {
-      favImgs.src = "../images/like-2.svg";
-    }
-  });
-}
-
-fetchData("https://5c782080f150df17.mokky.dev/items").then((data) => {
-  renderSneakers(data);
-  fetch("https://a57062685adfd13c.mokky.dev/erver")
-    .then((response) => response.json())
-    .then((data) => {
-      likedSneakers(data);
-    });
-});
+fetchData("https://a57062685adfd13c.mokky.dev/resurs")
+  .then((filteredData) => renderSneakers(filteredData))
+  .catch((error) =>
+    console.error("Произошла ошибка при получении и отображении данных:", error)
+  );
